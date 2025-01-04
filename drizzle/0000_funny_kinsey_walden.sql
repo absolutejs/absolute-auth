447a6c3ac32c14b789fd131bcc629a96ecd1e1d6
Alex Kahn <alexkahndev@gmail.com>
2025-01-04 16:40:29 -0500

    chore: update .gitignore and .npmignore to exclude 'build' and 'drizzle' directories; remove obsolete SQL and metadata files

--- drizzle/0000_funny_kinsey_walden.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"given_name" varchar(255),
-	"family_name" varchar(255),
-	"email" varchar(255),
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"auth_sub" varchar(255) PRIMARY KEY NOT NULL,
-	"picture" varchar(255)
-);
